1a5a9ba35c951b04b3369050815a5033020b75ad
zumichan <azumi.y.callanan@gmail.com>
2018-12-19 21:42:55 -0800

    Updated test

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,17 @@
+const request = require("request");
+const server = require("../../app");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+
+  describe("GET /", () => {
+    it("should return status code 200 and have 'Node Chat' in the body of the response", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("Node Chat");
+        done();
+      });
+    });
+  });
+
+});
